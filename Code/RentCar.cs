75e13070ea5bdb2d8b7dd9cec9cd64cf744998d9
Egor Grishchik <egorfog@gmail.com>
2021-12-16 16:44:03 +0300

    Add project files.

--- Code/RentCar.cs
+++ Code/RentCar.cs
@@ -12,13 +12,6 @@ namespace Kursach1
             CostOf1Hour = costOf1Hour;
         }
 
-        //public double Hours { get; set; }
-
-        //public override double CalculatedRevenue(double Hours)
-        //{
-        //    return Math.Round(CostOf1Hour * Hours, 2);
-        //}
-
         public override string ToString()
         {
             return $"{CarBrand} {YearOfIssue} {Mileage} {CostOf1Hour}";
